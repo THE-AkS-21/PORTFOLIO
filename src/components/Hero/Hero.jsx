6372ee7698398231fd470bc991a8d99acfaf3162
THE_AkS <93464264+THE-AkS-21@users.noreply.github.com>
2024-05-12 22:19:43 +0530

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+    return <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hi, I'm ANKIT</h1>
+            <p className={styles.description}>I'm a Software Engineer in its learning phase. I am open to expand my learnings and experience</p>
+            <a href="mailto:ankit.72816@gmail.com" className={styles.contactBtn}>Contact Me</a>
+        </div>
+        <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg}/>
+        <div className={styles.topBlur} />
+        <div className={styles.bottomBlur} />
+    </section>
+}
